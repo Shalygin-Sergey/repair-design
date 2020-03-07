141761c877c74124fee43cf9c7bdc585bc1acd99
Shalygin-Sergey <60192169+Shalygin-Sergey@users.noreply.github.com>
2020-03-08 02:19:26 +0300

    changed the file gulpfile.js, browser-sync it worked

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ gulp.task('hello', (done) => {
     done();
 });
 
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
